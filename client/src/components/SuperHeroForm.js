451c7f0fdb7fea75e7e8f5b42aa4ccc7645041d7
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2022-01-17 21:58:14 +0530

    clear referral codes after modal close

--- client/src/components/SuperHeroForm.js
+++ client/src/components/SuperHeroForm.js
@@ -45,6 +45,9 @@ export default function SuperHeroForm({
     setFirstName("");
     setLastName("");
     setEmail("");
+    setOnlineCourseRC("");
+    setLiveCourseRC("");
+    setOfflineCourseRC("");
   }
 
   return (
